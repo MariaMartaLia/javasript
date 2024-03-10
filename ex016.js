1c5185a62d2c8b9b4c32ccec56ba5e001ab9caf4
Maria Marta (Lia) <160326282+MariaMartaLia@users.noreply.github.com>
2024-03-10 10:14:32 -0300

    Create ex016.js

---
+++ ex016.js
@@ -0,0 +1,20 @@
+/*function parimpar(n) {
+    if (n % 2 == 0){
+    return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+} let res = parimpar(7)
+console.log (res)*/
+
+/* soma(n1=0,n2=0){
+    return n1+n2
+
+}
+console.log(soma(2, ))*/
+
+/*var v = function(x){
+    return x*2
+}
+ console.log (v(5))*/
+ 
